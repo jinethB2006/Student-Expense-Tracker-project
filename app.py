7e5dd6dc5693fedd1a23cf3e2e5364556efc5f5f
jineth b <jineth200655@gmail.com>
2026-03-12 09:14:45 +0530

    Implement delete expense history feature
    Added functionality to delete expense history.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from expense_manager import load_data,add_expense,save_budget,load_budget
+from expense_manager import load_data,add_expense,save_budget,load_budget,clear_expenses
 from datetime import date
 
 st.title("Student Expense Tracker")
@@ -38,6 +39,12 @@ df=load_data()
 st.header("All Expenses")
 st.dataframe(df)
 
+st.subheader("Manage Expenses")
+
+if st.button("Delete Expense History"):
+    clear_expenses()
+    st.success("All expenses deleted")
+    
 total=df["Amount"].sum()
 
 st.header("Summary")
@@ -45,5 +52,6 @@ st.header("Summary")
 st.write("Total Spent:",total)
 
 remaining=budget-total
+
 
 st.write("Remaining Budget:",remaining)
